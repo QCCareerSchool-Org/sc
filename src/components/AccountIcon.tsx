3af74f0030d91ee1185706010b47b769d9d9a8bd
Dave Welsh <dave@qccareerschool.com>
2022-03-07 11:30:18 -0500

    Fix alignment of account icon

--- src/components/AccountIcon.tsx
+++ src/components/AccountIcon.tsx
@@ -3,7 +3,7 @@ import { FaSignOutAlt } from 'react-icons/fa';
 
 export const AccountIcon = (): ReactElement => (
   <>
-    <a href="/students/accounts/view.php"><div id="accountImage" /></a>
+    <a href="/students/accounts/view.php"><div id="accountImage" className="me-md-3" /></a>
     <small className="d-none d-md-inline me-2">LOGOUT</small>
     <a href="/students/logout.php" className="d-none d-md-inline"><FaSignOutAlt /></a>
 
@@ -18,7 +18,6 @@ export const AccountIcon = (): ReactElement => (
         border-radius:28px;
         background:url('/students/portraits/view.php?thumbnail=64&amp;v=1590199169');
         background-size:cover;
-        margin-right: 1rem;
       }
     `}</style>
   </>
