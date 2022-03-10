4ef0b67e5b6e5a48a912ce2ba8c2c85fce428417
Dave Welsh <dave@qccareerschool.com>
2022-03-10 14:26:20 -0500

    Add comments to custom-next-env.d.ts file

--- custom-next-env.d.ts
+++ custom-next-env.d.ts
@@ -1,4 +1,9 @@
 /// <reference types="next" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+// NOTE: This should be kept up to date with changes to the automatically
+// generated next-env.d.ts, but with the reference to next/image-types/global
+// excluded.
+//
+// We have our own type file @types/images.d that overrides the module
+// declaration for *.svg. That file should also be kept up to date with
+// next/image-types/global.d.ts except for the changes to *.svg
