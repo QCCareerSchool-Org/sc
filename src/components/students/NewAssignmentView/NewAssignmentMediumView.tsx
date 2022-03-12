f5c8c476d846a75231f37de5d335240a387f860c
Dave Welsh <dave@qccareerschool.com>
2022-03-11 20:58:07 -0500

    Fix typo in media src

--- src/components/students/NewAssignmentView/NewAssignmentMediumView.tsx
+++ src/components/students/NewAssignmentView/NewAssignmentMediumView.tsx
@@ -12,15 +12,17 @@ type Props = {
 };
 
 export const NewAssignmentMediumView = ({ studentId, courseId, unitId, assignmentId, newAssignmentMedium }: Props): ReactElement | null => {
+  const src = `${endpoint}/students/${studentId}/courses/${courseId}/newUnits/${unitId}/assignments/${assignmentId}/media/${newAssignmentMedium.assignmentMediumId}/file`;
+
   if (newAssignmentMedium.type === 'image') {
     // eslint-disable-next-line @next/next/no-img-element
-    return <img src={`${endpoint}/students/${studentId}/courses/${courseId}/newUnits/${unitId}/assignment/${assignmentId}/media/${newAssignmentMedium.assignmentMediumId}/file`} className="mediaContent" alt={newAssignmentMedium.caption} />;
+    return <img src={src} className="mediaContent" alt={newAssignmentMedium.caption} />;
   }
 
   if (newAssignmentMedium.type === 'video') {
     return (
       <video controls className="mediaContent">
-        <source type={newAssignmentMedium.mimeTypeId} src={`${endpoint}/students/${studentId}/courses/${courseId}/newUnits/${unitId}/assignment/${assignmentId}/media/${newAssignmentMedium.assignmentMediumId}/file`} />
+        <source type={newAssignmentMedium.mimeTypeId} src={src} />
       </video>
     );
   }
@@ -28,7 +30,7 @@ export const NewAssignmentMediumView = ({ studentId, courseId, unitId, assignmen
   if (newAssignmentMedium.type === 'audio') {
     return (
       <audio controls>
-        <source type={newAssignmentMedium.mimeTypeId} src={`${endpoint}/students/${studentId}/courses/${courseId}/newUnits/${unitId}/assignment/${assignmentId}/media/${newAssignmentMedium.assignmentMediumId}/file`} />
+        <source type={newAssignmentMedium.mimeTypeId} src={src} />
       </audio>
     );
   }
