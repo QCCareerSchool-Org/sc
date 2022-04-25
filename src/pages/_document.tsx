2096e3682fb7e609fedfb2dda56c9e01ebaf824b
Dave Welsh <dave@qccareerschool.com>
2022-04-25 11:40:20 -0400

    Fix google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class QCDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:300,400,500,700,300italic,400italic,500italic,700italic|Material+Icons&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" />
         </Head>
         <body>
           <Main />
