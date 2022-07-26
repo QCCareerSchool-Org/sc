5e87e6251371a35999cb71d41b0927f5bfc19f75
Dave Welsh <dave@qccareerschool.com>
2022-07-26 08:59:32 -0400

    Update backend paths

--- src/basePath.ts
+++ src/basePath.ts
@@ -2,8 +2,8 @@ export const basePath = '/sc';
 
 export const endpoint = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8080/v1'
-  : 'https://studentcenter.qccareerschool.com/api/v1';
+  : 'https://studentcenter.qccareerschool.com/api/sc/v1';
 
 export const crmEndpoint = process.env.NODE_ENV === 'development'
   ? 'http://localhost:12080/v4'
-  : 'https://studentcenter.qccareerschool.com/crm/v4';
+  : 'https://studentcenter.qccareerschool.com/api/crm/v4';
