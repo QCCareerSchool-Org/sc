e8bc40bc59a9be0607b91824bfa1464632decc19
Dave Welsh <dave@qccareerschool.com>
2022-08-16 13:37:16 -0400

    Show the correct button text for initializing units

--- src/components/students/CourseView/index.tsx
+++ src/components/students/CourseView/index.tsx
@@ -2,7 +2,7 @@ import NextError from 'next/error';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type { MouseEvent, MouseEventHandler, ReactElement } from 'react';
-import { useCallback, useReducer } from 'react';
+import { useCallback, useMemo, useReducer } from 'react';
 
 import { endpoint } from '../../../basePath';
 import { CourseHeaderImage } from './CourseHeaderImage';
@@ -13,9 +13,8 @@ import { useInitialData } from './useInitialData';
 import { useInitializeNextUnit } from './useInitializeNextUnit';
 import { Section } from '@/components/Section';
 import { Spinner } from '@/components/Spinner';
-import type { NewUnit } from '@/domain/newUnit';
-import type { NewUnitTemplate } from '@/domain/newUnitTemplate';
 import { useStayLoggedIn } from '@/hooks/useStayLoggedIn';
+import type { EnrollmentWithStudentCourseAndUnits } from '@/services/students/enrollmentService';
 
 type Props = {
   studentId: number;
@@ -27,7 +26,7 @@ export const CourseView = ({ studentId, courseId }: Props): ReactElement | null 
   const [ state, dispatch ] = useReducer(reducer, initialState);
 
   // we're going to be linking to static resources that require the user to be
-  // logged in, and those resources don't have a refresh built-in mechanism
+  // logged in, and those resources don't have a refresh mechanism
   useStayLoggedIn();
 
   useInitialData(dispatch, studentId, courseId);
@@ -37,6 +36,8 @@ export const CourseView = ({ studentId, courseId }: Props): ReactElement | null 
   const handleNewUnitClick = useCallback((e: MouseEvent<HTMLTableRowElement>, unitId: string): void => {
     void router.push(router.asPath + '/units/' + unitId);
   }, [ router ]);
+
+  const nextUnit = useMemo(() => getNextUnit(state.enrollment), [ state.enrollment ]);
 
   if (state.error) {
     return <NextError statusCode={state.errorCode ?? 500} />;
@@ -77,8 +78,6 @@ export const CourseView = ({ studentId, courseId }: Props): ReactElement | null 
     return null;
   }
 
-  const nextUnit = getNextUnit(state.enrollment.course.newUnitTemplates, state.enrollment.newUnits);
-
   const handleInitializeButtonClick: MouseEventHandler<HTMLButtonElement> = () => {
     initializeNextUnit$.next({
       processingState: state.form.processingState,
@@ -100,17 +99,20 @@ export const CourseView = ({ studentId, courseId }: Props): ReactElement | null 
             </div>
             <div className="col-12 col-lg-6">
               <UnitsTable newUnits={state.enrollment.newUnits} onNewUnitClick={handleNewUnitClick} />
-              {nextUnit && (
-                <div className="d-flex align-items-center">
-                  <button onClick={handleInitializeButtonClick} className="btn btn-primary" style={{ width: 120 }}>
-                    {state.form.processingState === 'initializing'
-                      ? <Spinner size="sm" />
-                      : <>Start Unit {nextUnit}</>
-                    }
-                  </button>
-                  {state.form.processingState === 'initialize error' && <span className="text-danger ms-2">{state.form.errorMessage ?? 'initializing'}</span>}
-                </div>
-              )}
+              {nextUnit.success
+                ? (
+                  <div className="d-flex align-items-center">
+                    <button onClick={handleInitializeButtonClick} className="btn btn-primary" style={{ width: 120 }}>
+                      {state.form.processingState === 'initializing'
+                        ? <Spinner size="sm" />
+                        : <>Start Unit {nextUnit.unitLetter}</>
+                      }
+                    </button>
+                    {state.form.processingState === 'initialize error' && <span className="text-danger ms-2">{state.form.errorMessage ?? 'initializing'}</span>}
+                  </div>
+                )
+                : <NextUnitMessage nextUnit={nextUnit} />
+              }
             </div>
           </div>
         </div>
@@ -128,6 +130,66 @@ export const CourseView = ({ studentId, courseId }: Props): ReactElement | null 
   );
 };
 
-const getNextUnit = (newUnitTemplates: NewUnitTemplate[], newUnits: NewUnit[]): string | null => {
-  return 'A';
+const NextUnitMessage = ({ nextUnit }: { nextUnit: NextUnitResult }): ReactElement | null => {
+  if (nextUnit.success) {
+    return null;
+  }
+  switch (nextUnit.error) {
+    case 'loading':
+      return null;
+    case 'arrears':
+      return <p>Account is in arrears. Please contact the school.</p>;
+    case 'on hold':
+      return <p>Account is on hold. Please contact the school.</p>;
+    case 'incomplete':
+      return null;
+    case 'disabled':
+      return <p>This course is undergoing maintenance. Please check back in a few minutes.</p>;
+    case 'complete':
+      return <p>Assignments complete!</p>;
+  }
+};
+
+type NextUnitResult = {
+  success: true;
+  unitLetter: string;
+} | {
+  success: false;
+  error: 'loading' | 'arrears' | 'on hold' | 'incomplete' | 'disabled' | 'complete';
+};
+
+const getNextUnit = (enrollment?: EnrollmentWithStudentCourseAndUnits): NextUnitResult => {
+  if (!enrollment) {
+    return { success: false, error: 'loading' };
+  }
+
+  if (enrollment.student.arrears) {
+    return { success: false, error: 'arrears' };
+  }
+
+  if (enrollment.onHold) {
+    return { success: false, error: 'on hold' };
+  }
+
+  // make sure there are no open (unskipped or unmarked) units
+  if (!enrollment.newUnits.every(u => u.skipped || u.closed)) {
+    return { success: false, error: 'incomplete' };
+  }
+
+  if (!enrollment.course.newUnitsEnabled) {
+    return { success: false, error: 'disabled' };
+  }
+
+  const sortedUnitTemplates = enrollment.course.newUnitTemplates.sort((a, b) => {
+    if (a.order === b.order) {
+      return a.unitLetter.localeCompare(b.unitLetter);
+    }
+    return a.order - b.order;
+  });
+  for (const t of sortedUnitTemplates) {
+    if (!enrollment.newUnits.some(u => u.unitLetter === t.unitLetter)) {
+      return { success: true, unitLetter: t.unitLetter };
+    }
+  }
+  return { success: false, error: 'complete' };
 };
