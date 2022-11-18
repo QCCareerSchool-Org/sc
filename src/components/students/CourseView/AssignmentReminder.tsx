7ddec440f8bb9503bafc4f6be91f69517266719b
Dave Welsh <dave@qccareerschool.com>
2022-11-18 15:15:39 -0500

    Add cursor: pointer to assignment reminder link

--- src/components/students/CourseView/AssignmentReminder.tsx
+++ src/components/students/CourseView/AssignmentReminder.tsx
@@ -9,6 +9,7 @@ type Props = {
 
 export const AssignmentReminder: FC<Props> = ({ title, description }) => {
   const handleClick: MouseEventHandler<HTMLAnchorElement> = e => {
+    e.preventDefault();
     scrollToId('assignments');
   };
 
@@ -18,9 +19,12 @@ export const AssignmentReminder: FC<Props> = ({ title, description }) => {
         <div className="col-12 col-lg-4 col-xxl-3 mb-3 mb-lg-0">{title}</div>
         <div className="col-12 col-lg-5 col-xxl-6 mb-3 mb-lg-0" dangerouslySetInnerHTML={{ __html: description }} />
         <div className="col-8 col-sm-6 col-md-5 col-lg-3">
-          <a onClick={handleClick}><MaterialButton type="assignment">Go To Assignments</MaterialButton></a>
+          <a href="" className="assignmentLink" onClick={handleClick}><MaterialButton type="assignment">Go To Assignments</MaterialButton></a>
         </div>
       </div>
+      <style jsx>{`
+      .assignmentLink { text-decoration: none }
+      `}</style>
     </div>
   );
 };
