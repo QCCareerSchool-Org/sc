eac5a7ed359ad7323e3ff11306e395dcfe91519e
Dave Welsh <dave@qccareerschool.com>
2023-07-17 06:29:32 -0400

    Start the new layout

--- src/pages/students/courses/[courseId]/UnitAccordion.tsx
+++ src/pages/students/courses/[courseId]/UnitAccordion.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import type { FC } from 'react';
 import { Fragment, useEffect, useRef } from 'react';
 import { AiOutlineMinusCircle, AiOutlinePlusCircle } from 'react-icons/ai';
@@ -6,8 +7,10 @@ import type { Subject } from 'rxjs';
 import { AssignmentReminder } from './AssignmentReminder';
 import { Lesson } from './Lesson';
 import type { MaterialWithCompletionForm } from './state';
+import { NewSubmissionStatus } from './submissions/[submissionId]/NewSubmissionStatus';
 import type { MaterialCompleteEvent } from './useMaterialCompletion';
 import type { MaterialCompletion } from '@/domain/materialCompletion';
+import type { NewSubmission } from '@/domain/student/newSubmission';
 import type { Unit } from '@/domain/unit';
 import { useUnitToggleDispatch } from '@/hooks/useUnitToggleDispatch';
 import { useUnitToggleState } from '@/hooks/useUnitToggleState';
@@ -22,11 +25,13 @@ type Props = {
   materialCompletions: MaterialCompletion[];
   materialCompletion$: Subject<MaterialCompleteEvent>;
   firstUnit: boolean;
+  submission?: NewSubmission;
+  onNewUnitClick: (e: MouseEvent, submissionId: string) => void;
 };
 
 const iconSize = 24;
 
-export const UnitAccordion: FC<Props> = ({ studentId, enrollmentId, courseId, unit, materialCompletions, materialCompletion$, firstUnit }) => {
+export const UnitAccordion: FC<Props> = ({ studentId, enrollmentId, courseId, unit, materialCompletions, materialCompletion$, firstUnit, submission }) => {
   const unitToggleState = useUnitToggleState();
   const unitToggleDispatch = useUnitToggleDispatch();
   const firstRender = useRef(true);
@@ -55,7 +60,7 @@ export const UnitAccordion: FC<Props> = ({ studentId, enrollmentId, courseId, un
       <Separator />
       {open && (
         <div className="my-4">
-          {unit.materials.map((m, i) => {
+          {unit.materials.filter(m => m.type !== 'assignment').map((m, i) => {
             const complete = materialCompletions.some(mc => mc.materialId === m.materialId);
             return (
               <Fragment key={m.materialId}>
@@ -65,8 +70,51 @@ export const UnitAccordion: FC<Props> = ({ studentId, enrollmentId, courseId, un
               </Fragment>
             );
           })}
+          {submission
+            ? (
+              <>
+                <hr />
+                <div className="row py-4">
+                  <div className="col-12 col-lg-4 col-xxl-3 mb-3 mb-lg-0">
+                    {submission.skipped
+                      ? 'Skipped'
+                      : submission.closed
+                        ? 'Marked'
+                        : submission.submitted
+                          ? 'Submitted'
+                          : 'In Progress'
+                    }
+                  </div>
+                  <div className={`col-12 col-lg-5 col-xxl-6 mb-3 mb-lg-0 position-relative`}>
+                    <div>
+                      <h4 className="title h6 mb-2">Submission {submission.unitLetter}{submission.title && <>: {submission.title}</>}</h4>
+                      <NewSubmissionStatus studentId={studentId} courseId={courseId} newSubmission={submission} />
+                    </div>
+                  </div>
+                  <div className="col-8 col-sm-6 col-md-5 col-lg-3">
+                    <Link href={`${courseId}/submissions/${submission.submissionId}`}><a className="btn btn-primary">View</a></Link>
+                  </div>
+                </div>
+              </>
+            )
+            : (
+              <>
+              </>
+            )}
         </div>
       )}
+      <style jsx>{`
+      @media (min-width: 992px) {
+        .title {
+           font-size: 1.25rem;
+        }
+      }
+      @media (min-width: 1400px) {
+        .title {
+           font-size: 1.25rem;
+        }
+      }
+      `}</style>
     </>
   );
 };
