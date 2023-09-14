9e27f47aeb3b05bdaf3aba2bcc208516d678b175
Dave Welsh <dave@qccareerschool.com>
2023-09-14 14:37:12 -0400

    Remove crossorigin

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -30,7 +30,6 @@ export const Audio: FC<Props> = memo(props => {
       ref={audioRef}
       controls={props.controls}
       preload={props.preload}
-      crossOrigin="use-credentials"
       className={props.className}
       style={props.style}
       onError={handleError}
