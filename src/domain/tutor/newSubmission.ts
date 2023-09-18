405b43b8071520653cdb38efefe46a889f8d85bf
Dave Welsh <dave@qccareerschool.com>
2023-09-18 15:35:51 -0400

    Add responseProgress to all domain entities

--- src/domain/tutor/newSubmission.ts
+++ src/domain/tutor/newSubmission.ts
@@ -18,6 +18,7 @@ export type NewSubmission = {
   responseFilename: string | null;
   responseFilesize: number | null;
   responseMimeTypeId: string | null;
+  responseProgress: number | null;
   complete: boolean;
   points: number;
   mark: number | null;
