b52c222168a54ccdbfb63daa38454443c86d77f9
Dave Welsh <dave@qccareerschool.com>
2024-02-27 11:06:18 -0500

    Added link

--- src/pages/auditors/home.tsx
+++ src/pages/auditors/home.tsx
@@ -22,7 +22,7 @@ export const AuditorHome: FC<Props> = ({ auditorId }) => {
             <>
               <h1>Welcome to the Online Student Center, {state.auditor.firstName}!</h1>
               <p className="lead">You can access everything you need to monitor students' progress through the Online Student Center. Visit the &ldquo;Student&rdquo; tab to access a detailed view of each student's progress including course due dates and grades. The &ldquo;Account&rdquo; tab will allow you to change your email address or password.</p>
-              <a href="https://enroll.qcpetstudies.com/earthwise">Enroll New Students</a>
+              <a href="https://enroll.qcpetstudies.com/earthwise-accelerated-grooming" className="btn btn-primary btn-lg" target="_blank" rel="noreferrer">Enroll New Students</a>
             </>
           )
           : <Spinner />
