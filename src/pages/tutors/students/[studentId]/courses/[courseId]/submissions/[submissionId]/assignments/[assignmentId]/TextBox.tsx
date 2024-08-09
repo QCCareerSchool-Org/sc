da365f1840539c319e74e49d985ee0a72b0b25ce
Dave Welsh <dave@qccareerschool.com>
2024-08-09 12:19:40 -0400

    Added youtube link instructions for tutors

--- src/pages/tutors/students/[studentId]/courses/[courseId]/submissions/[submissionId]/assignments/[assignmentId]/TextBox.tsx
+++ src/pages/tutors/students/[studentId]/courses/[courseId]/submissions/[submissionId]/assignments/[assignmentId]/TextBox.tsx
@@ -13,8 +13,17 @@ type Props = {
 const lineHeight = 24;
 const padding = 14;
 
+const possibleYouTubeLink = /https:\/\/(?:.*\.)youtube\.com/ui;
+const goodYouTubeLink = /^https:\/\/www\.youtube\.com\/watch\?v=(?:[a-zA-Z0-9_-]){11,}/u;
+const youtubeShortLink = /https:\/\/www.youtube.com\/shorts\/([a-zA-Z0-9_-]){11,}/ui;
+
 export const TextBox: FC<Props> = ({ newTextBox, modified, submissionIsRedo }) => {
   const minHeight = (lineHeight * (newTextBox.lines ?? 3)) + padding;
+
+  const malformedYouTubeLink = possibleYouTubeLink.test(newTextBox.text) && !goodYouTubeLink.test(newTextBox.text);
+  const parts = youtubeShortLink.exec(newTextBox.text);
+  const videoId = parts ? parts[0] : null;
+
   return (
     <div className="row" id={newTextBox.textBoxId}>
       {newTextBox.description && <label className="form-label">{newTextBox.description}</label>}
@@ -26,6 +35,12 @@ export const TextBox: FC<Props> = ({ newTextBox, modified, submissionIsRedo }) =
           return <Fragment key={i}><br /><br />{p}</Fragment>;
         })}</div>
         {submissionIsRedo && modified && <div className="fw-bold text-danger">(CHANGED)</div>}
+        {malformedYouTubeLink && (
+          <div>({videoId
+            ? <>This appears to be a link to a YouTube Short. You can use the video ID specified and instead visit <a href={`https://www.youtube.com/watch?v=${videoId}`} target="_blank" rel="noopener noreferrer">https://www.youtube.com/watch?v={videoId}</a></>
+            : <>This appears to be a non-standard YouTube link. You can examine the link to find the 8-digit video ID and instead visit https://www.youtube.com/watch?v=&lt;videoId&gt;</>
+          })</div>
+        )}
       </div>
     </div>
   );
