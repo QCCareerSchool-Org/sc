822342565baa125aae075c541dcd0e5c8ec0291a
Dave Welsh <dave@qccareerschool.com>
2025-03-25 11:25:35 -0400

    Added March banners

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -3,14 +3,14 @@
 import type { FC } from 'react';
 import { useMemo, useReducer } from 'react';
 
-import DesignDesktopImage from './2025/01/design/desktop-register.jpg';
-import DesignMobileImage from './2025/01/design/mobile-register.jpg';
-import PetDesktopImage from './2025/01/pet/desktop-register.jpg';
-import PetMobileImage from './2025/01/pet/mobile-register.jpg';
 import EventDesktopImage from './2025/02/event/desktop-register.jpg';
 import EventMobileImage from './2025/02/event/mobile-register.jpg';
 import MakeupDesktopImage from './2025/02/makeup/desktop-register.jpg';
 import MakeupMobileImage from './2025/02/makeup/mobile-register.jpg';
+import DesignDesktopImage from './2025/03/design/desktop-register.jpg';
+import DesignMobileImage from './2025/03/design/mobile-register.jpg';
+import PetDesktopImage from './2025/03/pet/desktop-register.jpg';
+import PetMobileImage from './2025/03/pet/mobile-register.jpg';
 
 import { Inner } from './inner';
 import { initialState, reducer } from './state';
@@ -52,20 +52,40 @@ export const Banner: FC = () => {
 
   const now = new Date().getTime();
 
-  if (hasPetCourses && now < Date.UTC(2025, 0, 23, 23, 30)) { // 2025-01-23T18:30 (23:30 UTC)
-    return <Inner backgroundColor="#02013f" desktopSrc={PetDesktopImage.src} mobileSrc={PetMobileImage.src} url="https://event.webinarjam.com/register/74/kznpvb9z" />;
+  if (hasPetCourses && now < Date.UTC(2025, 2, 27, 22)) { // 2025-03-27T18:00 (22:00 UTC)
+    return <Inner
+      backgroundColor="#02013f"
+      desktopSrc={PetDesktopImage.src}
+      mobileSrc={PetMobileImage.src}
+      url="https://event.webinarjam.com/register/82/7yp6rtkr"
+    />;
   }
 
   if (hasEventCourses && now < Date.UTC(2025, 1, 19, 23, 30)) { // 2025-01-15T18:30 (23:30 UTC)
-    return <Inner backgroundColor="#02013f" desktopSrc={EventDesktopImage.src} mobileSrc={EventMobileImage.src} url="https://event.webinarjam.com/register/72/qq0o4b85" />;
+    return <Inner
+      backgroundColor="#02013f"
+      desktopSrc={EventDesktopImage.src}
+      mobileSrc={EventMobileImage.src}
+      url="https://event.webinarjam.com/register/72/qq0o4b85"
+    />;
   }
 
   if (hasMakeupCourses && now < Date.UTC(2025, 1, 18, 23, 30)) { // 2025-01-14T18:30 (23:30 UTC)
-    return <Inner backgroundColor="#02013f" desktopSrc={MakeupDesktopImage.src} mobileSrc={MakeupMobileImage.src} url="https://event.webinarjam.com/register/71/8ym3ptyo" />;
+    return <Inner
+      backgroundColor="#02013f"
+      desktopSrc={MakeupDesktopImage.src}
+      mobileSrc={MakeupMobileImage.src}
+      url="https://event.webinarjam.com/register/71/8ym3ptyo"
+    />;
   }
 
-  if (hasDesignCourses && now < Date.UTC(2025, 0, 14, 21, 30)) { // 2025-01-21T18:30 (23:30 UTC)
-    return <Inner backgroundColor="#02013f" desktopSrc={DesignDesktopImage.src} mobileSrc={DesignMobileImage.src} url="https://event.webinarjam.com/register/73/1y86ot3p" />;
+  if (hasDesignCourses && now < Date.UTC(2025, 2, 25, 24)) { // 2025-03-25T18:00 (22:00 UTC)
+    return <Inner
+      backgroundColor="#02013f"
+      desktopSrc={DesignDesktopImage.src}
+      mobileSrc={DesignMobileImage.src}
+      url="https://event.webinarjam.com/register/83/4yk21t76"
+    />;
   }
 
   return null;
