59f42ec378435a151e3bc7bcb609e6c6be016d8a
Daphne Huang <daphnehlj25@gmail.com>
2026-06-11 09:29:12 -0700

    bug fixes and remove unnecessary imports

--- src/providers/ServicesProvider.tsx
+++ src/providers/ServicesProvider.tsx
@@ -3,25 +3,16 @@ import { createContext, useState } from 'react';
 
 import { certificateService, crmCountryService, crmProvinceService, crmTelephoneCountryCodeService, gradeService, loginService, passwordResetRequestService, uuidService, videoService } from '../services';
 import type { ICertificateService } from '@/services/certificateService';
-import { CertificateService } from '@/services/certificateService';
 import type { ICRMCountryService } from '@/services/crmCountryService';
-import { CRMCountryService } from '@/services/crmCountryService';
 import type { ICRMProvinceService } from '@/services/crmProvinceService';
-import { CRMProvinceService } from '@/services/crmProvinceService';
 import type { ICRMTelephoneCountryCodeService } from '@/services/crmTelephoneCountryCodeService';
-import { CRMTelephoneCountryCodeService } from '@/services/crmTelephoneCountryCodeService';
 import type { IGradeService } from '@/services/gradeService';
-import { GradeService } from '@/services/gradeService';
 import type { IHttpService } from '@/services/httpService';
 import { AxiosHttpService } from '@/services/httpService';
 import type { ILoginService } from '@/services/loginService';
-import { LoginService } from '@/services/loginService';
 import type { IPasswordResetRequestService } from '@/services/passwordResetRequestService';
-import { PasswordResetRequestService } from '@/services/passwordResetRequestService';
 import type { IUUIDService } from '@/services/uuidService';
-import { UUIDService } from '@/services/uuidService';
 import type { IVideoService } from '@/services/videoService';
-import { VideoService } from '@/services/videoService';
 import { instance } from 'src/axiosInstance';
 
 export interface Services {
@@ -45,7 +36,7 @@ interface Props {
 
 export const ServicesProvider: FC<Props> = ({ children }) => {
   const [ state ] = useState({
-    httpService: AxiosHttpService,
+    httpService: new AxiosHttpService(instance),
     loginService,
     passwordResetRequestService,
     uuidService,
